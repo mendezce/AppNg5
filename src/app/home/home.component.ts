3353d8a20de04ba225d647d2210a31f104aa95a2
mendezce86 <mendezce@hotmail.com>
2018-03-12 15:52:20 -0400

    firts commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
+import { trigger, style, query, transition, animate, keyframes, stagger } from '@angular/animations'
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+  // animations : [
+  //   trigger('goals', [
+  //     transition('* => *', [
+  //       query(':enter', style({ opacity: 0}), {optional : true}),
+  //       query(':enter', stagger('300ms', [
+  //         animate('.6s ease-in', keyframes([
+  //         //  style({opacity : 0, transform: 'translateY(-75%)', offset: 0}),
+  //           style({opacity : .5, transform: 'translateY(35px)', offset: 3}),
+  //         //  style({opacity : 1, transform: 'translateY(0)', offset: 1})
+  //         ]))]), {optional: true})
+  //     ])
+  //   ])
+  // ]
+})
+export class HomeComponent implements OnInit {
+
+  goals: any;
+
+  constructor(private data: DataService) { }
+
+  ngOnInit() {
+    this.data.goal.subscribe(res => this.goals = res);
+  }
+
+}
